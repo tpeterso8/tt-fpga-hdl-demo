4695cb0039b851e48ad3fce379ac38c6cbfbebe6
tpeterso8 <161381619+tpeterso8@users.noreply.github.com>
2024-03-04 16:10:56 -0500

    Update top.sv

--- src/top.sv
+++ src/top.sv
@@ -4,7 +4,7 @@
 //_\SV
    // Include Tiny Tapeout Lab.
    // Included URL: "https://raw.githubusercontent.com/os-fpga/Virtual-FPGA-Lab/35e36bd144fddd75495d4cbc01c4fc50ac5bde6f/tlv_lib/tiny_tapeout_lib.tlv"// Included URL: "https://raw.githubusercontent.com/os-fpga/Virtual-FPGA-Lab/a069f1e4e19adc829b53237b3e0b5d6763dc3194/tlv_lib/fpga_includes.tlv"
-//_\source top.tlv 99
+//_\source top.tlv 105
 
 //_\SV
 
@@ -41,7 +41,7 @@ module top(input logic clk, input logic reset, input logic [31:0] cyc_cnt, outpu
    // Instantiate the Tiny Tapeout module.
    tt_um_template tt(.*);
 
-   assign passed = top.cyc_cnt > 40;
+   assign passed = top.cyc_cnt > 400;
    assign failed = 1'b0;
 endmodule
 
@@ -223,7 +223,7 @@ logic FpgaPins_Fpga_COUNT_reset_a0;
 //_\TLV
    /* verilator lint_off UNOPTFLAT */
    // Connect Tiny Tapeout I/Os to Virtual FPGA Lab.
-   //_\source /raw.githubusercontent.com/osfpga/VirtualFPGALab/35e36bd144fddd75495d4cbc01c4fc50ac5bde6f/tlvlib/tinytapeoutlib.tlv 76   // Instantiated from top.tlv, 168 as: m5+tt_connections()
+   //_\source /raw.githubusercontent.com/osfpga/VirtualFPGALab/35e36bd144fddd75495d4cbc01c4fc50ac5bde6f/tlvlib/tinytapeoutlib.tlv 76   // Instantiated from top.tlv, 174 as: m5+tt_connections()
       assign L0_slideswitch_a0[7:0] = ui_in;
       assign L0_sseg_segment_n_a0[6:0] = ~ uo_out[6:0];
       assign L0_sseg_decimal_point_n_a0 = ~ uo_out[7];
@@ -231,7 +231,7 @@ logic FpgaPins_Fpga_COUNT_reset_a0;
    //_\end_source
 
    // Instantiate the Virtual FPGA Lab.
-   //_\source /raw.githubusercontent.com/osfpga/VirtualFPGALab/a069f1e4e19adc829b53237b3e0b5d6763dc3194/tlvlib/fpgaincludes.tlv 307   // Instantiated from top.tlv, 171 as: m5+board(/top, /fpga, 7, $, , my_design)
+   //_\source /raw.githubusercontent.com/osfpga/VirtualFPGALab/a069f1e4e19adc829b53237b3e0b5d6763dc3194/tlvlib/fpgaincludes.tlv 307   // Instantiated from top.tlv, 177 as: m5+board(/top, /fpga, 7, $, , my_design)
       
       //_\source /raw.githubusercontent.com/osfpga/VirtualFPGALab/a069f1e4e19adc829b53237b3e0b5d6763dc3194/tlvlib/fpgaincludes.tlv 355   // Instantiated from /raw.githubusercontent.com/osfpga/VirtualFPGALab/a069f1e4e19adc829b53237b3e0b5d6763dc3194/tlvlib/fpgaincludes.tlv, 309 as: m4+thanks(m5__l(309)m5_eval(m5_get(BOARD_THANKS_ARGS)))
          //_/thanks
@@ -255,7 +255,12 @@ logic FpgaPins_Fpga_COUNT_reset_a0;
                      assign FpgaPins_Fpga_COUNT_cntbtn_a0 = ui_in[0];
                      assign FpgaPins_Fpga_COUNT_edge_a0 = FpgaPins_Fpga_COUNT_cntbtn_a1 == 0 && FpgaPins_Fpga_COUNT_cntbtn_a0 ;
             
-                     assign FpgaPins_Fpga_COUNT_amount_a0[3:0] =  FpgaPins_Fpga_COUNT_reset_a0 ? 0 : (FpgaPins_Fpga_COUNT_amount_a1 + {3'b0, FpgaPins_Fpga_COUNT_edge_a0} );
+                     assign FpgaPins_Fpga_COUNT_amount_a0[3:0] =  FpgaPins_Fpga_COUNT_reset_a0 ? 0 :
+                                     !FpgaPins_Fpga_COUNT_edge_a0 ? FpgaPins_Fpga_COUNT_amount_a1:
+                                     FpgaPins_Fpga_COUNT_amount_a1 == 4'b1001 ? 4'b0:
+                                     (FpgaPins_Fpga_COUNT_amount_a1 + 4'b1);
+            
+            
             
                      //$btn[3:0] = *ui_in[3:1];
                      //$sel_digit = *clk;
@@ -263,6 +268,7 @@ logic FpgaPins_Fpga_COUNT_reset_a0;
                      //$ones[6:0] = 7'b1001111;
             
                      assign FpgaPins_Fpga_COUNT_digit_a0[3:0] = FpgaPins_Fpga_COUNT_amount_a0[3:0];
+            
                      assign uo_out[7:0] =
                                 FpgaPins_Fpga_COUNT_digit_a0 == 4'h0
                                    ? 8'b00111111:
@@ -284,7 +290,7 @@ logic FpgaPins_Fpga_COUNT_reset_a0;
                                    ? 8'b01111111:
                                 FpgaPins_Fpga_COUNT_digit_a0 == 4'h9
                                    ? 8'b01101111:
-                                8'b00000110;
+                                8'b00111111;
             
             
                //$sel_digit ? {1, $tens} : {0, $ones};
@@ -334,7 +340,7 @@ logic FpgaPins_Fpga_COUNT_reset_a0;
       
    //_\end_source
    // Label the switch inputs [0..7] (1..8 on the physical switch panel) (top-to-bottom).
-   //_\source /raw.githubusercontent.com/osfpga/VirtualFPGALab/35e36bd144fddd75495d4cbc01c4fc50ac5bde6f/tlvlib/tinytapeoutlib.tlv 82   // Instantiated from top.tlv, 173 as: m5+tt_input_labels_viz(⌈"UNUSED", "UNUSED", "UNUSED", "UNUSED", "UNUSED", "UNUSED", "UNUSED", "UNUSED"⌉)
+   //_\source /raw.githubusercontent.com/osfpga/VirtualFPGALab/35e36bd144fddd75495d4cbc01c4fc50ac5bde6f/tlvlib/tinytapeoutlib.tlv 82   // Instantiated from top.tlv, 179 as: m5+tt_input_labels_viz(⌈"UNUSED", "UNUSED", "UNUSED", "UNUSED", "UNUSED", "UNUSED", "UNUSED", "UNUSED"⌉)
       for (input_label = 0; input_label <= 7; input_label++) begin : L1_InputLabel //_/input_label
          
       end
